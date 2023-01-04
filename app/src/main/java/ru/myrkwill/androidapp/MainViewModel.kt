073daab208e383c56cbd64543818a49e48d2ae47
Mark Nagibin <myrkwill@icloud.com>
2023-01-04 18:03:42 +0300

    Add model and table layout

--- app/src/main/java/ru/myrkwill/androidapp/MainViewModel.kt
+++ app/src/main/java/ru/myrkwill/androidapp/MainViewModel.kt
@@ -2,8 +2,9 @@ package ru.myrkwill.androidapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.myrkwill.androidapp.adapter.WeatherModel
 
 class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
