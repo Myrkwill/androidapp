5fbced1b332a56849a1e27c055782ba8e253b612
Mark Nagibin <myrkwill@icloud.com>
2023-01-04 16:30:37 +0300

    Add view

--- app/src/main/java/ru/myrkwill/androidapp/fragment/MainFragment.kt
+++ app/src/main/java/ru/myrkwill/androidapp/fragment/MainFragment.kt
@@ -1,15 +1,19 @@
 package ru.myrkwill.androidapp.fragment
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import ru.myrkwill.androidapp.R
 import ru.myrkwill.androidapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -18,6 +22,24 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
