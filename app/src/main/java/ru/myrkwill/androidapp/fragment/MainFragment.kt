512f75e48d1658bfadee68c3e617a5d5e94cece2
Mark Nagibin <myrkwill@icloud.com>
2023-01-05 16:31:12 +0300

    Add parse weather data

--- app/src/main/java/ru/myrkwill/androidapp/fragment/MainFragment.kt
+++ app/src/main/java/ru/myrkwill/androidapp/fragment/MainFragment.kt
@@ -2,6 +2,7 @@ package ru.myrkwill.androidapp.fragment
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +11,17 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayoutMediator
+import org.json.JSONObject
 import ru.myrkwill.androidapp.R
 import ru.myrkwill.androidapp.adapter.VpAdapter
+import ru.myrkwill.androidapp.adapter.WeatherModel
 import ru.myrkwill.androidapp.databinding.FragmentMainBinding
+
+const val API_KEY = "999fe75cf7934588b45125034230301"
 
 class MainFragment : Fragment() {
 
@@ -42,6 +50,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding) {
@@ -50,6 +59,74 @@ class MainFragment : Fragment() {
         TabLayoutMediator(tabLayout, vp) { tab, pos ->
             tab.text = tList[pos]
         }.attach()
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?" +
+                "key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(Request.Method.GET, url, { result ->
+            parseWeatherData(result)
+        }, { error ->
+            Log.d("MyLog", "Error: $error")
+        })
+        queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel> {
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()) {
+            val day = daysArray[i] as JSONObject
+
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition").getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel) {
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.time}")
+        Log.d("MyLog", "condition: ${item.condition}")
+        Log.d("MyLog", "Temp: ${item.currentTemp}")
+        Log.d("MyLog", "icon: ${item.imageUrl}")
+        Log.d("MyLog", "maxTemp: ${item.maxTemp}")
+        Log.d("MyLog", "minTemp: ${item.minTemp}")
+        Log.d("MyLog", "hours: ${item.hours}")
     }
 
     private fun permissionListener() {
